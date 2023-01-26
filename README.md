e5b2d5584e93b8a535626b6739c0f8d0fc22c74b
Raditya Harya <radityaharya02@gmail.com>
2023-01-26 17:30:32 +0000

    add ps

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ bash -c "$(curl -sSL https://about.radityaharya.me)"
 ```bash
 npx radityaharya
 ```
+
+```ps1
+iwr https://about.radityaharya.me | iex
+```
+
+```powershell
+iwr https://about.radityaharya.me/ps | iex
+```
+
+```sh
---
+++ aboutme.ps1
@@ -0,0 +1,25 @@
+$sentences = @(
+    "Hi $env:USERNAME, Thanks for running this seemingly suspicious script",
+    "don't worry, it's absolutely safe",
+    "or is it?",
+    "I'm just kidding, it's safe",
+    "My name is Raditya Harya, I'm an Information Systems Student at Bina Nusantara University",
+    "Here are some of my contact details",
+    "Email: contact@radityaharya.me",
+    "Github: https://github.com/radityaharya.me",
+    "LinkedIn: https://www.linkedin.com/in/radityaharya/"
+)
+
+foreach ($sentence in $sentences) {
+    if ($sentence -like ">*") {
+        $sentence = $sentence.Substring(1)
+        Write-Host -ForegroundColor Yellow $sentence
+    } else {
+        Write-Host -ForegroundColor White -BackGroundColor Black $sentence
+    }
+    Start-Sleep -Seconds 1
+}
+
+while($true) {
+    Start-Sleep -Seconds 1
+}
